890b4cb3612081a911e3d51376b97bb1886c8a02
Sofian05 <sofian.morariu05@gmail.com>
2020-04-09 21:10:38 +0300

    "salut"

--- maze.py
+++ maze.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import pygame as py
+import pygame
 import random
-from pygame.locals import *
+#from pygame.locals import *
 
 BLOCK_SIZE = 20
 WHITE = (255,255,255)
@@ -10,6 +10,39 @@ RED = (255,0,0)
 GREEN = (0,255,0)
 BLACK = (0,0,0)
 
-def display(self, widht, height):
-    py.display.set_mode((widht * BLOCK_SIZE, height * BLOCK_SIZE))
-    window.fill(BLACK)
+width = 40
+height = 45
+
+class Game:
+
+    def __init__(self):
+        self.screen = pygame.display.set_mode((width * BLOCK_SIZE, height * BLOCK_SIZE))
+        pygame.display.set_caption('Splash Labyrinth')
+        pygame.time.Clock().tick(60)
+        print("1")
+        
+
+    def update(self):
+        pass
+
+    def run(self):
+        while True:
+            self.draw()
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        pygame.display.update()
+        print(2)
+
+
+
+def main():
+    pygame.display.init()
+
+    game = Game()
+    game.run()
+    pygame.display.quit()
+
+
+
+    
