fa2eb01bece903ce1791896d892f1d8e88e505d7
tomover9000 <vlad.zorlescu@gmail.com>
2020-04-22 13:16:38 +0300

    added size prompt and size limits

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from maze import Game
 SIZE = 30 
 def main() :
     print("Player 1 use Space \nPlayer 2 use Right Arrow")
-    SIZE = int(input("Insert Playing size: ")) 
+    SIZE = int(input("Insert Playing size (20 - 100): ")) 
+    if SIZE < 20 :
+        SIZE = 20
+    if SIZE > 100 :
+        SIZE = 100
     DISPLAY_SIZE = int(input("Insert display size (marimea laturii patratului ferestrei): "))
     play_style = input("Game mode: (R)andom/(G)rid: ")
     pygame.display.init()
